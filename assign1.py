c8c8bb6bcf8697ae823e7302ada5ddfc33cb3779
avijeetpandey28 <70476406+avijeetpandey28@users.noreply.github.com>
2022-09-12 02:02:57 +0530

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,21 @@
+print("hello python")
+
+print("hello" , "python", sep='\n')
+
+print("\"MySirG\"")
+
+print("\"Teacher's Day\"")
+
+x = ("Avijeet Pandey")
+print(x)
+
+a = ("Avijeet Pandey")
+b = ("M.TECH")
+c = 27
+d = 5
+print(a,b,c,d)
+
+a = 20 
+b = 30
+c = 40
+print(a,b,c,sep='\n')
