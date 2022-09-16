e8fddeabc8b532cd2c2f96955ee1f9f34e2878c1
avijeetpandey28 <70476406+avijeetpandey28@users.noreply.github.com>
2022-09-16 15:07:23 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,56 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#1 Write a python script to convert a number into str type.
+a = 20
+print(type(str(a)))
+<class 'str'>
+
+#ans2
+#Write a python script to print Unicode of the character ‘m’
+print(ord('m'))
+109
+
+#ans3 Write a python script to print character representation of a given unicode 100.
+print(chr(100))
+d
+
+#ans4 Write a python script to print any number and its binary equivalent
+b = 30
+print(bin(b))
+0b11110
+
+#ans5
+KeyboardInterrupt
+KeyboardInterrupt
+#ans5 5. Write a python script to print any number and its octal equivalent.
+c = 13
+print(c,oct(c))
+13 0o15
+
+#ans6Write a python script to print any number and its hexadecimal equivalent.
+d = 15
+print(d,hex(d))
+15 0xf
+
+#ans7 Write a python script to store binary number 1100101 in a variable and print it in decimal format.
+e = 0b1100101
+print(e)
+101
+
+#ans8
+#Write a python script to store a hexadecimal number 2F in a variable and print it in octal format.
+g = 0x2F
+print(oct(g))
+0o57
+
+#ans9
+#Write a python script to store an octal number 125 in a variable and print it in binary format.
+h = 0o125
+print(bin(h))
+0b1010101
+
+#ans10 Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and display the result in binary format.
+x = 0o25
+y = 0x39
+print(bin(x)+ bin(y))
+0b101010b111001
