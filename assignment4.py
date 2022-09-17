304b38e1b69cf766249a2ef337baf9223e36b611
avijeetpandey28 <70476406+avijeetpandey28@users.noreply.github.com>
2022-09-17 15:25:31 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,128 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#1. Write a python script to take your name as input from the user and then print it.
+#ans1
+name = input("Enter your name : ")
+Enter your name : avijeet
+print(name)
+avijeet
+#2 Write a python script to take input from the user. Input must be a number.
+number = int(input("number: "))
+number: 9
+print(number)
+9
+#3 Write a python script which takes two numbers from the user, then calculate their sum and display the result.
+number1 = int(input("number1: "))
+number1: 32
+number2 = int(input("number2: "))
+number2: 37
+print(number1 + number2)
+69
+#4
+#Write a python script which takes the radius from the user and display area of a circle.
+radius = float(int(enter the radius: ))
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+radius = float(int("enter the radius: "))
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    radius = float(int("enter the radius: "))
+ValueError: invalid literal for int() with base 10: 'enter the radius: '
+radius = float(innput("enter the radius: "))
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    radius = float(innput("enter the radius: "))
+NameError: name 'innput' is not defined. Did you mean: 'input'?
+radius = float(input("enter the radius: "))
+enter the radius: 3.2
+area = 3.14*r*r
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    area = 3.14*r*r
+NameError: name 'r' is not defined
+area = 3.14*radius*radius
+print(area)
+32.153600000000004
+
+#5
+#Write a python script to calculate the square of a number. Number is entered by the user.
+num = int(input("enter the number: "))
+enter the number: 32
+square = num**num
+print(square)
+1461501637330902918203684832716283019655932542976
+square = num**2
+print(square)
+1024
+#6 Write a python script to calculate the area of Triangle. Number is entered by the user.
+base = float(input("enter the base: "))
+enter the base: 29
+height = float(input("enter the height: "))
+enter the height: 28
+area = 0.5 *base*height
+print(area)
+406.0
+
+#7Write a python script to calculate average of three numbers, entered by the user
+num1=float(input("enter the first number: "))
+enter the first number: 12
+num2=float(input("enter the second number: "))
+enter the second number: 92
+num3=float(input("enter the third number: "))
+enter the third number: 124
+average = (num1+num2+num3)/3
+print(average)
+76.0
+#8 Write a python script to calculate simple interest
+principal = float(input("enter the principal:  "))
+enter the principal:  10000
+rate = float(input("enter the rate:  "))
+enter the rate:  78
+time = float(input("enter the time:  "))
+enter the time:  24
+simple interest = (principal*rate*time)/100
+SyntaxError: invalid syntax
+simpleinterest = (principal*rate*time)/100
+print(simpleinterest)
+187200.0
+
+#9Write a python script to calculate the volume of a cuboid.
+length = float(input("enter the length:  " )
+breadth= float(input("enter the breadth:  " )
+               
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+breadth = float(input("enter the breadth :  " )
+                
+height =  float(input("enter the height :  " )
+                
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+length = float(input("enter the length: "))
+                
+enter the length: 34
+breadth = float(input("enter the breadth: "))
+                
+enter the breadth: 93
+height =
+                
+SyntaxError: invalid syntax
+
+height = float(input("enter the height: "))
+                
+enter the height: 21
+volume = length*breadth*height
+                
+print(volume)
+                
+66402.0
+#10Write a python script to calculate area of a rectangle
+                
+length = float(input("enter the length: "))
+                
+enter the length: 21
+breadth = float(input("enter the breadth: "))
+                
+enter the breadth: 32
+area = length * breadth
+                
+print(area)
+                
+672.0
